eca6eb97f8b06cd051bf64239d1543638972f4a4
vilhota30 <nirodalina@gmail.com>
2023-04-23 18:54:36 +0300

    вносить зміни у файл js\02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+ const galleryListEl = document.querySelector('.gallery');
+console.log(galleryListEl);
+
+const listElemMarkup = createElemOfListMarkup(galleryItems);
+galleryListEl.insertAdjacentHTML("beforeend", listElemMarkup);
+ 
+
+function createElemOfListMarkup(galleryItems) {
+      return galleryItems.map(({ preview, original, description }) => {
+         return `
+         <li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+          <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+           />
+         </a>
+         </li>
+         `;
+         })
+          .join('');
+     
+}
+
+const galleryLightBox = new SimpleLightbox(`.gallery__link`, {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+console.log(galleryLightBox);
